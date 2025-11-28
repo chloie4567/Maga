bb5aef49692f9cef12bce7264cd7f43cce72c2b1
chloie4567 <maranon_chloie@fbc.ph.education>
2025-11-28 18:04:17 +0800

    Add toggleLearn function to show/hide learn section

---
+++ script.java
@@ -0,0 +1,8 @@
+function toggleLearn() {
+    var section = document.getElementById("learn-section");
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
