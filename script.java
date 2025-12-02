4a5158943edea6d4e3890ea1a9b7d2fea7f7cd45
chloie4567 <maranon_chloie@fbc.ph.education>
2025-12-02 16:12:09 +0800

    Fix toggleLearn function display logic

--- script.java
+++ script.java
@@ -1,8 +1,10 @@
  function toggleLearn() {
-var learnSection = document.getElementById("learn-section");
-if (learnSection.style.display === "none") {
-learnSection.style.display = "block";
-} else {
-learnSection.style.display = "none";
+  var learnSection = document.getElementById('learn-section');
+  if (learnSection.style.display === 'block') {
+    learnSection.style.display = 'none';
+  } else {
+    learnSection.style.display = 'block';
+  }
 }
- }
+
+
