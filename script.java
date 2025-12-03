692702c41a7794185bb4d284af28b57d0a7573e1
chloie4567 <maranon_chloie@fbc.ph.education>
2025-12-03 17:24:32 +0800

    Fix formatting and add closing brace in script.java

--- script.java
+++ script.java
@@ -1,6 +1,7 @@
  const learnButton = document.querySelector('.button');
 const learnSection = document.getElementById('learn-section');
+
 learnButton.addEventListener('click', () => {
   learnSection.classList.toggle('show');
 });
-
+}
