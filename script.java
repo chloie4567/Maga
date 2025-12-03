63c31ba235f432d7840cba435a5de80ae92fbbdb
chloie4567 <maranon_chloie@fbc.ph.education>
2025-12-03 17:21:54 +0800

    Fix formatting of event listener code

--- script.java
+++ script.java
@@ -1,6 +1,6 @@
  const learnButton = document.querySelector('.button');
 const learnSection = document.getElementById('learn-section');
-
 learnButton.addEventListener('click', () => {
   learnSection.classList.toggle('show');
 });
+
